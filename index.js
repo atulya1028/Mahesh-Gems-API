92285653a8dfb508b6ef1bb3a11026c4a7544d65
atulya1028 <atulyaagrawal1996@gmail.com>
2025-04-16 14:10:13 +0530

    Nodemailer

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
+
 
 // Routes
 const authRoutes = require("./routes/authRoutes");
