50790f685316e02ff2b4131d47f61e5fb2cc3e94
atulya1028 <atulyaagrawal1996@gmail.com>
2025-04-24 14:49:00 +0530

    Updates

--- models/Jewelry.js
+++ models/Jewelry.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
-const jewelrySchema = new mongoose.Schema({
-  title: String,
-  price: String,
-  image: String,
-  description: String,
-});
+const jewelrySchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true }, // Match external API's string id
+    name: { type: String, required: true }, // Changed from title to name
+    price: { type: Number, required: true }, // Changed to Number
+    image: { type: String, required: true },
+    description: { type: String },
+    category: { type: String }, // Added for completeness
+  },
+  { collection: "jewelry" } // Explicitly set collection name
+);
 
 module.exports = mongoose.model("Jewelry", jewelrySchema);
